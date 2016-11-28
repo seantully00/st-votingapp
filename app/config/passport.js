15adac0b5472d9d154fdcc6ab4571a9133f564c0
Sean <seantully00@hotmail.com>
2016-11-28 03:15:36 +0000

    twitter auth

--- app/config/passport.js
+++ app/config/passport.js
@@ -21,7 +21,7 @@ module.exports = function (passport) {
     	consumerSecret: configAuth.twitterAuth.consumerSecret,
     	callbackURL: configAuth.twitterAuth.callbackURL
 	},
-	function(token, tokenSecret, profile, cb) {
+	function(token, tokenSecret, profile, done) {
 		process.nextTick(function () {
 			User.findOne({ 'twitter.id': profile.id }, function (err, user) {
 				if (err) {
