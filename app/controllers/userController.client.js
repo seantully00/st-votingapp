ced1b3fc34e79a53a47a8fea65b8d6425bc18248
Sean <seantully00@hotmail.com>
2016-11-29 02:31:14 +0000

    profile

--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -16,17 +16,17 @@
       var userObject = JSON.parse(data);
 
       if (userObject.displayName !== null) {
-         updateHtmlElement(userObject, displayName, 'twitter.name');
+         updateHtmlElement(userObject, displayName, twitter.name);
       } else {
-         updateHtmlElement(userObject, displayName, 'twitter.screen_name');
+         updateHtmlElement(userObject, displayName, twitter.screen_name);
       }
 
       if (profileId !== null) {
-         updateHtmlElement(userObject, profileId, 'twitter.id');   
+         updateHtmlElement(userObject, profileId, twitter.id);   
       }
 
       if (profileUsername !== null) {
-         updateHtmlElement(userObject, profileUsername, 'twitter.screen_name');   
+         updateHtmlElement(userObject, profileUsername, twitter.screen_name);   
       }
 
       if (profileRepos !== null) {
