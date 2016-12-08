3ef2c64e7689f78cb08d5083af8206df442c44ec
Sean <seantully00@hotmail.com>
2016-12-08 01:29:24 +0000

    add poll choices

--- app/controllers/createpollController.client.js
+++ app/controllers/createpollController.client.js
@@ -19,7 +19,7 @@ function removeElement(elementId) {
 
 function addChoice() {
     choiceId++; // increment fileId to get a unique ID for the new element
-    var html = 'Choice 2:<br><input type="text" id="choice' + choiceId + '"><br><button type = "button" onclick="javascript:removeElement(choice' + choiceId + ')">Remove</button>';
+    var html = 'Choice 2:<br><input type="text" id="choice' + choiceId + '"><br><button type = "button" onclick="removeElement(choice' + choiceId + ');">Remove</button>';
     addElement("createpoll", 'input', 'choice' + choiceId, html);
 };
 
